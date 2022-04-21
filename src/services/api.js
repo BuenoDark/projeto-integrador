b2ed00bf0dca9fc50edf315b6e2fdef0060c6142
Fran <francieliceleghim2013@gmail.com>
2022-04-20 23:25:27 -0300

    bug fix categoria

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "http://ec2-18-118-21-85.us-east-2.compute.amazonaws.com:8080/",
 });
 
 export default api;
